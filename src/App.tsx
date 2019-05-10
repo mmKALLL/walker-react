1c0f2b554c195313225ba5a77b64e13789c82678
Esa Koskinen <esa.koskinen@reaktor.co.jp>
2019-05-11 02:25:52 +0900

    feat: add font loading to App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <link href="https://fonts.googleapis.com/css?family=Charm|Gloria+Hallelujah|Noto+Sans+JP|Roboto+Slab" rel="stylesheet"></link>
         <MainMenu startNewGame={this.startNewGame} />
       </div>
     )
