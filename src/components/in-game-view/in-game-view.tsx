ecc79f8a1682aa770fa65ccbf458ab478a17dc26
Esa Koskinen <esa.koskinen@reaktor.co.jp>
2019-07-03 11:28:22 +0900

    feat: add mood handling and bg color transitions

--- src/components/in-game-view/in-game-view.tsx
+++ src/components/in-game-view/in-game-view.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import FooterArea from '../footer-area/footer-area';
 import StatusArea from '../status-area/status-area';
 import ActionPanel from '../action-panel/action-panel';
+import MoodHandler from '../mood-handler/mood-handler';
 
 interface State {
   stepCount: number
   position: number
+  mood: Mood
 }
 
 export default class InGameView extends React.Component<{}, State> {
@@ -14,6 +16,12 @@ export default class InGameView extends React.Component<{}, State> {
     this.state = {
       stepCount: 0,
       position: 0, // Can step both forward and back
+      mood: {
+        overall: 40,
+        r: 0,
+        g: 0,
+        b: 0
+      }
     }
   }
 
@@ -28,8 +36,15 @@ export default class InGameView extends React.Component<{}, State> {
             }))}
             />
         <FooterArea />
+        <MoodHandler mood={ this.state.mood } />
       </div>
     )
   }
 }
 
+export type Mood = {
+  overall: number // 0-155. General mood level.
+  r: number // 0-100. Love, passion, bonds.
+  g: number // 0-100. Optimism, wellbeing, altruism.
+  b: number // 0-100. Rationality, thinking, calmness.
+}
